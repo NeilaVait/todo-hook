59482bd23000e37ce50134a2392dd0c39f260b3b
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-09 13:07:42 +0300

    second way of done undone

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,27 @@ function App() {
   const [todos, setTodos] = useState([])
 
   const setDoneUndone = (id) => {
-    console.log('setDoneUndone', id);
+    // console.log('setDoneUndone', id);
+    // id yra id to todo itemo kurio done dali norim pakeisti i ptriesinga
+    // pasidarom todos kopija
+    // surasti ta todo objekta
+    // pakeisti jo done dali
+    // nustatom todos su setTodos i kopija
+
+    // one way
+    // const todosCopy = [...todos];
+    // const foundTodo = todosCopy.find(i => i.id === id);
+    // foundTodo.done = !foundTodo.done;
+    // setTodos(todosCopy)
+    
+    // second way
+    const todosCopy = todos.map(i => {
+      return i.id === id ? {...i, done: !i.done} : {...i}
+    })
+    setTodos(todosCopy)
+
+
+    
   }
 
   useEffect(() => {
