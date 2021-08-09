0d1c47fbae57bff3429274c53b0145d19f48514d
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-09 11:00:35 +0300

    external todo file, useEffect to fill values

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './App.css';
 import { TodoList } from './components/TodoList';
+import allTodos from './db/todos'
 
 function App() {
+  // console.log(allTodos());
   const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    setTodos(allTodos())
+  }, [])
   
   return (
     <div className="App">
